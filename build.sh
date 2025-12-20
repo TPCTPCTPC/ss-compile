e5289fb3bc5ebecfca00990819e6dc39cd2ca6bc
TPCTPCTPC <32757790+TPCTPCTPC@users.noreply.github.com>
2025-12-20 23:37:18 +0800

    Modify build.sh to update Cargo.toml and clone repo
    Updated build script to modify Cargo.toml profiles and clone the latest version of shadowsocks-rust.

--- build.sh
+++ build.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 set -e
 
-export CARGO_PROFILE_RELEASE_LTO="fat"
-export CARGO_PROFILE_RELEASE_CODEGEN_UNITS=1
-export CARGO_PROFILE_RELEASE_PANIC="abort"
-export CARGO_PROFILE_RELEASE_STRIP="true"
-export CARGO_PROFILE_RELEASE_OPT_LEVEL=3
-export CARGO_PROFILE_RELEASE_INCREMENTAL="false"
-
 git clone https://github.com/shadowsocks/shadowsocks-rust.git
 cd shadowsocks-rust
 
 LATEST_TAG=$(curl -s https://api.github.com/repos/shadowsocks/shadowsocks-rust/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 git checkout "$LATEST_TAG"
+
+sed -i 's/\[profile.release\]/\[profile.trash\]/' Cargo.toml
+
+cat >> Cargo.toml <<EOF
+[profile.release]
+lto = "fat"
+codegen-units = 1
+panic = "abort"
+strip = true
+opt-level = 3
+incremental = false
+EOF
 
 cargo build --release --no-default-features --features "server,aead-cipher-2022" --bin ssserver
 
