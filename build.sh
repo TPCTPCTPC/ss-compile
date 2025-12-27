e5ed265df75e0a83ee19bfe8510c6cc8b7a4bddb
TPCTPCTPC <32757790+TPCTPCTPC@users.noreply.github.com>
2025-12-27 11:21:20 +0800

    Refactor build.sh for improved environment setup

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -e
 
-# Rust 环境配置
+# Rust Env
 export CARGO_PROFILE_RELEASE_LTO="fat"
 export CARGO_PROFILE_RELEASE_CODEGEN_UNITS=1
 export CARGO_PROFILE_RELEASE_PANIC="abort"
 export CARGO_PROFILE_RELEASE_STRIP="true"
 export CARGO_PROFILE_RELEASE_OPT_LEVEL=3
 export CARGO_PROFILE_RELEASE_INCREMENTAL="false"
+
+# Go Env
+export GOAMD64=v3
+export CGO_ENABLED=0
 
 # 1. Build Shadowsocks-rust
 git clone https://github.com/shadowsocks/shadowsocks-rust.git ss-src
@@ -29,18 +33,28 @@ strip -s target/release/shadow-tls
 mv target/release/shadow-tls ../shadow-tls
 cd ..
 
-# 3. Build Node Exporter (Go v3)
-export GOAMD64=v3
-export CGO_ENABLED=0
-git clone --depth 1 https://github.com/prometheus/node_exporter.git node-src
+# 3. Build Node Exporter
+git clone https://github.com/prometheus/node_exporter.git node-src
 cd node-src
+NODE_TAG=$(curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+git checkout "$NODE_TAG"
 go build -ldflags="-s -w" -o ../node_exporter
 cd ..
 
-# 4. Build Blackbox Exporter (Go v3)
-git clone --depth 1 https://github.com/prometheus/blackbox_exporter.git black-src
+# 4. Build Blackbox Exporter
+git clone https://github.com/prometheus/blackbox_exporter.git black-src
 cd black-src
+BLACK_TAG=$(curl -s https://api.github.com/repos/prometheus/blackbox_exporter/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+git checkout "$BLACK_TAG"
 go build -ldflags="-s -w" -o ../blackbox_exporter
 cd ..
 
-echo "✅ All builds finished: ./ssserver, ./shadow-tls, ./node_exporter, ./blackbox_exporter"
+# 5. Build Mosdns
+git clone https://github.com/IrineSistiana/mosdns.git mosdns-src
+cd mosdns-src
+MOS_TAG=$(curl -s https://api.github.com/repos/IrineSistiana/mosdns/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+git checkout "$MOS_TAG"
+go build -ldflags="-s -w -X main.version=$MOS_TAG" -trimpath -o ../mosdns
+cd ..
+
+echo "All builds finished."
